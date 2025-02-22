c635b9591003e06a4205a1a2ffeb4a489a9f4787
Luherrerap <herreraparral320@gmail.com>
2025-02-22 11:56:22 -0500

    Add files via upload

---
+++ Funcion1.java
@@ -0,0 +1,24 @@
+package Funciones;
+
+public class Funcion1 {
+    public static int Suma(int num1, int num2){
+
+        return num1+num2;
+    }
+
+
+    public static void main(String[] args){
+
+        int resultado = Suma(1, 2);
+        System.out.println(resultado);
+
+
+
+
+
+
+
+
+    }
+    
+}
