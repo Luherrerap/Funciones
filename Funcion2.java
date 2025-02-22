f316283c46403b3300b0da7dc5e7eca1f325e453
Luherrerap <herreraparral320@gmail.com>
2025-02-22 12:35:33 -0500

    Add files via upload

---
+++ Funcion2.java
@@ -0,0 +1,17 @@
+package Funciones;
+
+public class Funcion2 {
+
+    public static double division(int num1, int num2){
+        return num1/num2;
+    }
+
+    public static void main(String[] args){
+
+        double resultado = division(25, 3);
+        System.out.println(resultado);
+
+
+    }
+    
+}
